ed5c05c32957034ffe79341edca8153ae9ce408a
Bridget Klingler <bridget.klingler@gmail.com>
2019-07-25 16:31:58 -0400

    set up the home.js for when the backend is up and running (hopefully) and set up the export lines -with defaults- for all js pages; rearranged the navBar components for flow

--- parcel-frontend/js/components/home.js
+++ parcel-frontend/js/components/home.js
@@ -1,5 +1,17 @@
-export default function Home(){
+export default function Home(stellarBodies){
     return`
     <h1>Space Program Mission Tracker</h1>
-        <p>This site shows you all the different missions to outspace worldwide. It's divided up by Agency, Mission, and Stellar Body. Check out all the times we mere mortals have expanded our research into the great unknown.</p>`
+        <p>This site shows you all the different missions to outspace worldwide. It's divided up by Agency, Mission, and Stellar Body. Check out all the times we mere mortals have expanded our research into the great unknown.</p>
+        <ul id='stellarbody-list'>
+            ${stellarBodies.map(stellarBody =>{
+                return `    
+                    <li>
+                        <img src='${stellarBody.imageURL}'></img>
+                        <h4>${stellarBody.name}</h4>
+                        <input class='stellarBody_id' type='hidden' value='${stellarBody.name}
+                    </li>
+                    `;
+            }).join("")}
+        </ul>
+        `;
 };
