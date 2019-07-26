170fc809e79783421b15323d8cbb7b9df1518de7
CLux <cplux7@gmail.com>
2019-07-26 11:56:39 -0400

    populated remaining controllers

--- Backend/SpaceProgramMissionTracker/Controller/MissionController.cs
+++ Backend/SpaceProgramMissionTracker/Controller/MissionController.cs
@@ -1,11 +1,60 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaceProgramMissionTracker.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SpaceProgramMissionTracker.Controller
 {
-    public class MissionController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MissionController : ControllerBase
     {
+        private SiteContext db;
+
+        public MissionController(SiteContext db)
+        {
+            this.db = db;
+        }
+
+        // GET api/Missions
+        [HttpGet]
+        public ActionResult<IEnumerable<Mission>> Get()
+        {
+            return db.Missions;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Mission> GetById(int id)
+        {
+            return db.Missions.Single(a => a.MissionId == id);
+        }
+
+        // POST api/Mission
+        [HttpPost]
+        public ActionResult<IEnumerable<Mission>> Post([FromBody] Mission mission)
+        {
+            db.Missions.Add(mission);
+            db.SaveChanges();
+            return db.Missions.ToList();
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        public ActionResult<IEnumerable<Mission>> Put([FromBody] Mission mission)
+        {
+            db.Missions.Update(mission);
+            db.SaveChanges();
+            return db.Missions.ToList();
+        }
+
+        [HttpDelete]
+        public ActionResult<IEnumerable<Mission>> Delete(Mission mission)
+        {
+            db.Missions.Remove(mission);
+            db.SaveChanges();
+            return db.Missions.ToList();
+        }
     }
 }
