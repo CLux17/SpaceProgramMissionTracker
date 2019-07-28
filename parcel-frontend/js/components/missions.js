fbac35187a3d381fed49f96ab4f0e46bfcefcbd0
Bridget Klingler <bridget.klingler@gmail.com>
2019-07-28 16:33:39 -0400

    added the add function for the agency list; working on drop down option to add a mission with the values of agencyId and SBid;

--- parcel-frontend/js/components/missions.js
+++ parcel-frontend/js/components/missions.js
@@ -1,4 +1,27 @@
-export default function Missions(){
+export default function Missions(missions){
     return`
-        <p>This is the Mission Page</p>`
+    <ul id="mission-list">
+    ${missions.map(mission => {
+            return`
+            <li>
+                <h2>${mission.name}</h2>
+                <img class='mission_img' src='${mission.imageURL}' alt='${mission.name} image'/>
+                <h4>${mission.description}</h4>
+            </li>`
+
+        }).join("")}
+        </ul>
+            <section class='add-mission'>
+                <input class='add-mission_name' type='text' placeholder='Add Mission name...'>
+                <input class='add-mission_img' type='text' placeholder='Add Image...'>
+                <input class='add-mission_descrip' type='text' placeholder='Add Description...'>
+                <div>
+                <select id="select">
+                <option value="${missions.agencyId}">int</option>
+            </select> </div>       
+                <button class="add-mission_submit multibutton">Add Mission</button>
+        
+        
+                <input class='mission_Id' type='hidden' value='${missions.Id}>
+            `
 };
