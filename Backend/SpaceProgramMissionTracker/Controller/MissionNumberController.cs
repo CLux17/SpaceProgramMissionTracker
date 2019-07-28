515351df79f1f521636d9ee3d3fbaac6a53febe3
CLux17 <cplus7@gmail.com>
2019-07-27 21:50:16 -0400

    updated and saved new controller

--- Backend/SpaceProgramMissionTracker/Controller/MissionNumberController.cs
+++ Backend/SpaceProgramMissionTracker/Controller/MissionNumberController.cs
@@ -1,11 +1,60 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaceProgramMissionTracker.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SpaceProgramMissionTracker.Controller
 {
-    public class MissionNumberController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MissionNumberController : ControllerBase
     {
+        private SiteContext db;
+
+        public MissionNumberController(SiteContext db)
+        {
+            this.db = db;
+        }
+
+        // GET api/MissionNumbers
+        [HttpGet]
+        public ActionResult<IEnumerable<MissionNumber>> Get()
+        {
+            return db.MissionNumbers;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<MissionNumber> GetById(int id)
+        {
+            return db.MissionNumbers.Single(a => a.MissionNumberId == id);
+        }
+
+        // POST api/MissionNumber
+        [HttpPost]
+        public ActionResult<IEnumerable<MissionNumber>> Post([FromBody] MissionNumber missionnumber)
+        {
+            db.MissionNumbers.Add(missionnumber);
+            db.SaveChanges();
+            return db.MissionNumbers.ToList();
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        public ActionResult<IEnumerable<MissionNumber>> Put([FromBody] MissionNumber missionnumber)
+        {
+            db.MissionNumbers.Update(missionnumber);
+            db.SaveChanges();
+            return db.MissionNumbers.ToList();
+        }
+
+        [HttpDelete]
+        public ActionResult<IEnumerable<MissionNumber>> Delete(MissionNumber missionnumber)
+        {
+            db.MissionNumbers.Remove(missionnumber);
+            db.SaveChanges();
+            return db.MissionNumbers.ToList();
+        }
     }
 }
