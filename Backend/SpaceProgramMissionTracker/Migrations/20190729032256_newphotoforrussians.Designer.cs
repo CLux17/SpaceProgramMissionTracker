07929177e8c90dbc53b10c707048155d140f1806
CLux17 <cplus7@gmail.com>
2019-07-28 23:23:47 -0400

    found image for russian mission migration included

--- Backend/SpaceProgramMissionTracker/Migrations/20190729032256_newphotoforrussians.Designer.cs
+++ Backend/SpaceProgramMissionTracker/Migrations/20190729032256_newphotoforrussians.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SpaceProgramMissionTracker;
 
 namespace SpaceProgramMissionTracker.Migrations
 {
     [DbContext(typeof(SiteContext))]
-    partial class SiteContextModelSnapshot : ModelSnapshot
+    [Migration("20190729032256_newphotoforrussians")]
+    partial class newphotoforrussians
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
